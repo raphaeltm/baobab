bb5d564411dbc4685e557305458f48bfa971bb51
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2016-11-14 13:57:34 -0800

    email

--- setup.py
+++ setup.py
@@ -3,5 +3,11 @@ from setuptools import setup
 setup(
     name="baobab",
     version="0.1",
-    description="Barebones templating system and static site generator for web designers."
+    description="Barebones templating system and static site generator for web designers.",
+    url="https://github.com/raphaeltm/baobab",
+    author="Raphaël Titsworth-Morin",
+    author_email="info@raphaeltm.com",
+    license="MIT",
+    packages=['baobab'],
+    zip_safe=False,
 )
